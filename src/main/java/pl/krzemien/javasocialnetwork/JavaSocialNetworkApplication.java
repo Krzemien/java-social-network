5467ec8a2c2b2f121819d1a22867c74afa6ad53b
Filip Krzymiński <filipkrzym@gmail.com>
2021-07-08 11:10:34 +0200

    init

---
+++ src/main/java/pl/krzemien/javasocialnetwork/JavaSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package pl.krzemien.javasocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/krzemien/javasocialnetwork/JavaSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzemien.javasocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

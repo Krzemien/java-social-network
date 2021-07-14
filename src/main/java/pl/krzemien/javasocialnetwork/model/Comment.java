8e3468211ad6f95987a49ac66367f426d6fcb855
Filip Krzymiński <filipkrzym@gmail.com>
2021-07-14 08:27:02 +0200

    added Comment class

---
+++ src/main/java/pl/krzemien/javasocialnetwork/model/Comment.java
@@ -0,0 +1,25 @@
+package pl.krzemien.javasocialnetwork.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Post post;
+
+}

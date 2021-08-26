7328227dec26aef5c06f5822a54a0b0883f89555
Filip Krzymiński <filipkrzym@gmail.com>
2021-08-27 01:29:15 +0200

    principal refactored

--- src/main/java/pl/krzemien/javasocialnetwork/controller/PostController.java
+++ src/main/java/pl/krzemien/javasocialnetwork/controller/PostController.java
@@ -5,26 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.krzemien.javasocialnetwork.model.Post;
-import pl.krzemien.javasocialnetwork.model.User;
 import pl.krzemien.javasocialnetwork.service.PostService;
-import pl.krzemien.javasocialnetwork.service.UserService;
 
 import javax.validation.Valid;
-import java.security.Principal;
 
 @Controller
 public class PostController {
-    private final UserService userService;
     private final PostService postService;
 
-    public PostController(UserService userService, PostService postService) {
-        this.userService = userService;
+    public PostController(PostService postService) {
         this.postService = postService;
     }
 
     @PostMapping("/post")
-    public String postValid(Principal principal, @Valid Post post, BindingResult result, Model model){
-        User user = userService.getUserByEmail(principal.getName());
+    public String postValid(@Valid Post post, BindingResult result, Model model){
 
         if (result.hasErrors()){
             model.addAttribute("posts", postService.getAllPosts());
@@ -32,7 +26,6 @@ public class PostController {
             return "index";
         }
 
-        post.setUser(user);
         postService.addPost(post);
 
         return "redirect:/";
--- src/main/java/pl/krzemien/javasocialnetwork/service/PostService.java
+++ src/main/java/pl/krzemien/javasocialnetwork/service/PostService.java
@@ -1,21 +1,27 @@
 package pl.krzemien.javasocialnetwork.service;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pl.krzemien.javasocialnetwork.model.Post;
+import pl.krzemien.javasocialnetwork.model.User;
 import pl.krzemien.javasocialnetwork.repository.PostRepository;
 
+import java.security.Principal;
 import java.util.Collections;
 import java.util.List;
 
 @Service
 public class PostService {
     private final PostRepository postRepository;
+    private final UserService userService;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserService userService) {
         this.postRepository = postRepository;
+        this.userService = userService;
     }
 
     public void addPost(Post post){
+        post.setUser(getPrincipal());
         postRepository.save(post);
     }
 
@@ -23,6 +29,16 @@ public class PostService {
         List<Post> posts = postRepository.findAll();
         Collections.reverse(posts);
         return posts;
+    }
+
+    public User getPrincipal(){
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        MyUserDetails myUserDetails = (MyUserDetails) principal;
+//        myUserDetails.getUser();
+
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.getUserByEmail(principal.getName());
+        return user;
     }
 
 }
